36a80e97d352de7ff43eddde409e49df1dd7c1f8
ericnoharaleclair <eric.noharaleclair@gmail.com>
2024-08-25 01:32:12 -0500

    simple chat app with only global chat capabilities

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
 const express = require("express");
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.listen(process.env.PORT_NUMBER);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat msg", (msg) => {
+    io.emit("chat msg", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("http://localhost:3000");
+});
